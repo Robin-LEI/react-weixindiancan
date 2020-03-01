a52a3ed8635274d92675fb033176602ad23faae2
robin lei <13661603945@163.com>
2020-03-01 14:46:19 +0800

    '完成订单相关接口'

--- mock/index.js
+++ mock/index.js
@@ -2,9 +2,11 @@
 const welcome = require('./api/welcome')
 const project = require('./api/project')
 const addShopCar = require('./api/shopCar')
+const order = require('./api/order')
 
 module.exports = (app) => {
   app.use('/api/welcome', welcome)
   app.use('/api/project', project)
   app.use('/api/project', addShopCar)
+  app.use('/api/project', order)
 }
